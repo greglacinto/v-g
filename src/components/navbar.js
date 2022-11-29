79e49a8420f6f08f14362b5f471119e19afa6666
Gregory Odiase <gregory.odiase@cwg-plc.com>
2022-11-29 09:03:45 +0100

    refractor: returned the navbar.js file back to a function and used the useState hook

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,46 +1,41 @@
 import '../styles/navbar.css' 
 import {Link} from 'react-router-dom';
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-class NavBar extends Component {
-    state = {
-        clicked: false,
+function NavBar() {
+
+    const [isClicked, setIsClicked] = useState(false)
+
+    function handleClick() {
+        setIsClicked(!isClicked)
     }
+    
+    return (
+        <nav>
+            <ul className={isClicked ? 
+            "#nav-links active-m" : "#nav-links inactive-m"} 
+            >
+                <Link to={`/`}
+                style={{ textDecoration: 'none' }}>
+                    <li>Home</li>
+                </Link>
+                <li>Music Requests</li>
+                <Link to={`/menu`} 
+                style={{ textDecoration: 'none' }}>
+                    <li>Menu</li>
+                </Link>
 
-    handleClick = () => {
-        this.setState({
-            clicked: !this.state.clicked,
-        })
-    }
+            </ul>
 
-    render() {
-        return (
-            <nav>
-                <ul className={this.state.clicked ? 
-                "#nav-links active-m" : "#nav-links inactive-m"} 
-                >
-                    <Link to={`/`}
-                    style={{ textDecoration: 'none' }}>
-                        <li>Home</li>
-                    </Link>
-                    <li>Music Requests</li>
-                    <Link to={`/menu`} 
-                    style={{ textDecoration: 'none' }}>
-                        <li>Menu</li>
-                    </Link>
-
-                </ul>
-
-                <div id="mobile" onClick={this.handleClick}>
-                        <i id="bar"
-                        className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}
-                        >
-                        </i>
-                       
-                </div>
-            </nav>
-        );
-    }
+            <div id="mobile" onClick={handleClick}>
+                    <i id="bar"
+                    className={isClicked ? 'fas fa-times' : 'fas fa-bars'}
+                    >
+                    </i>
+                    
+            </div>
+        </nav>
+    );
 }
 
 export default NavBar;
